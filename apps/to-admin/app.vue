b0c7bf32dd264e96e71c559e8b6bebe68035b87e
kurukuruz <kurukuruz@local>
2023-11-03 21:53:30 +0900

    add: 共通コンポーネントを作成

--- apps/to-admin/app.vue
+++ apps/to-admin/app.vue
@@ -3,5 +3,10 @@
     <h1>管理者向けページ</h1>
     <MainVisual />
     <CaseConverter />
+    <Footer />
   </div>
 </template>
+
+<script setup>
+import Footer from "@kurukuruz/shared-components/src/components/DefaultFooter.vue"
+</script>
---
+++ packages/shared-components/src/components/DefaultFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="theFooter">Footer</div>
+</template>
+
+<style scoped>
+.theFooter {
+    text-align: center;
+    color: #ddd;
+    background-color: #222;
+}
+</style>
