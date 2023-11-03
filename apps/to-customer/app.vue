3334658b7e8f995db5fb0d354ef7ea16844e6465
kurukuruz <kurukuruz@local>
2023-11-03 22:13:36 +0900

    add: 共通コンポーネントの利用を追加

--- apps/to-customer/app.vue
+++ apps/to-customer/app.vue
@@ -3,5 +3,10 @@
     <h1>お客様向けページ</h1>
     <MainVisual />
     <ClickCounter />
+    <Footer />
   </div>
 </template>
+
+<script setup>
+import Footer from "@kurukuruz/shared-components/src/components/DefaultFooter.vue"
+</script>
