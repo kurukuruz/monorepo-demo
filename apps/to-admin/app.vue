e220830542e00e70f684d2f881ceb1b4220de683
kurukuruz <kurukuruz@local>
2023-11-03 21:01:02 +0900

    add: ToAにコンポーネントを追加

--- apps/to-admin/app.vue
+++ apps/to-admin/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>管理者向けページ</h1>
+    <MainVisual />
   </div>
 </template>
---
+++ apps/to-admin/components/MainVisual.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mainVisual">
+        管理者向けコンテンツ
+    </div>
+</template>
+
+<style scoped>
+.mainVisual {
+    height: 10rem;
+    background-color: #afa;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
