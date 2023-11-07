699671730e16f3bfd020417d29afa59c5f873300
kurukuruz <kurukuruz@local>
2023-11-08 01:06:44 +0900

    modify: ハンバーガーメニューのコンポーネントを切り出し

---
+++ apps/to-admin/components/HamburgerButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="hamburgerWrapper" @click="toggleState">
+        <HamburgerIcon :is-open="isOpen" />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    isOpen: Boolean
+})
+const emit = defineEmits(["'update:isOpen'"])
+
+const toggleState = () => {
+    emit('update:isOpen', !props.isOpen)
+}
+</script>
+
+<style scoped>
+.hamburgerWrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    background-color: #ffa;
+}
+</style>
---
+++ apps/to-admin/components/HamburgerIcon.vue
@@ -0,0 +1,52 @@
+<template>
+    <span class="hambuger" :class="{'open': isOpen}"></span>
+</template>
+
+<script setup>
+defineProps({
+    isOpen: Boolean
+})
+</script>
+
+<style scoped>
+.hambuger {
+    display: block;
+    position: relative;
+    width: 20px;
+    height: 2px;
+    background-color: #000;
+    transition: all .4s;
+}
+
+.hambuger.open {
+    background-color: inherit;
+}
+
+.hambuger::before, .hambuger::after {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background-color: #000;
+    transition: all .4s;
+}
+
+.hambuger::before {
+    top: -6px;
+}
+
+.hambuger.open::before {
+    transform: rotate(45deg);
+    top: 0px
+}
+
+.hambuger::after {
+    bottom: -6px;
+}
+
+.hambuger.open::after {
+    transform: rotate(-45deg);
+    bottom: 0px;
+}
+</style>
